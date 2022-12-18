ff313e95764cadb479c017809438b98d8e34b2b7
Виктор Галилюк <galilyuk.v@oneway.su>
2022-12-18 18:01:02 +0300

    красивый инпут

--- sandbox/front/SitesFoto/lk.php
+++ sandbox/front/SitesFoto/lk.php
@@ -8,11 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!--    <link rel="preconnect" href="https://fonts.gstatic.com">-->
-    <!--    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">-->
     <link rel="stylesheet" type="text/css" href="custom.css">
+<!--    подключаем vue-->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.8/dist/vue.js"></script>
     <title>Фронт фото</title>
 </head>
 <body>
@@ -134,18 +134,39 @@
 
 
         </div>
-        <div class="col-6" style="background-color: aquamarine">
-            1111
+        <div class="col-6">
+            <img src="image/1.jpg" class="vikAvatarImg img-fluid w-75"/>
+            <p></p>
+            <div class="text-field text-field_floating">
+                <label class="vikInput-file" id="app">
+                    <span class="vikInput-file-text" type="text">{{message}}</span>
+                    <input type="file" name="file" v-on:change="previewFiles">
+                    <span class="VikInput-file-btn">Выберите файл</span>
+                </label>
+                <p>Ваш аватар</p>
+            </div>
+
+
         </div>
     </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: null,
+        },
+        methods: {
+            previewFiles(event) {
+                console.log(event.target.files[0].name);
+                this.message = event.target.files[0].name;
+            },
+        },
+    })
+</script>
 </body>
 </html>
 
