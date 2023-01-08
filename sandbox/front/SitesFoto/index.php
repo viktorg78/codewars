1654be1b0c2fe14123de8cab52888eb4a5da15dc
Виктор Галилюк <galilyuk.v@oneway.su>
2023-01-08 21:44:57 +0300

    лайки

--- sandbox/front/SitesFoto/index.php
+++ sandbox/front/SitesFoto/index.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
 
 <!--    <link rel="preconnect" href="https://fonts.gstatic.com">-->
@@ -106,16 +106,135 @@
 </div>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-4" style="background-color: aqua">
-            1111
-        </div>
-        <div class="col-4" style="background-color: aquamarine">
-            1111
-        </div>
-        <div class="col-4" style="background-color: blueviolet">
-            1111
+        <div class="col-12" style="background-color: #2fc6f7">
+            поиск
         </div>
     </div>
+
+<!--    залайканые фото-->
+    <div class="row">
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 start-0 vikBlur2 p-1">
+<!--                        <i class="bi bi-star-fill"></i> фото понравилось-->
+                        <i class="bi bi-star"></i>
+                    </div>
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-hand-thumbs-down"></i>
+<!--                    <i class="bi bi-hand-thumbs-down-fill"></i>  поставили дизлайк-->
+                        <i class="bi bi-hand-thumbs-up"></i> 10
+<!--                    <i class="bi bi-hand-thumbs-up-fill"></i> поставленный лайк-->
+
+                    </div>
+                </div>
+                <div class="card-body">
+                   <p class="card-text text-center text-truncate" title="Название название название">
+                       Название название название
+                   </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-stars"></i> 10
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-center text-truncate" title="Название название название">
+                        Название название название
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-stars"></i> 10
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-center text-truncate" title="Название название название">
+                        Название название название
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-stars"></i> 10
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-center text-truncate" title="Название название название">
+                        Название название название
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-stars"></i> 10
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-center text-truncate" title="Название название название">
+                        Название название название
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card m-1 position-relative">
+                <div class="position-relative">
+                    <img src="image/1.jpg"  class="card-img-top w-100" alt="Название фото">
+                    <div class="position-absolute bottom-0 end-0 p-1 vikBlur">
+                        <i class="bi bi-stars"></i> 10
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-center text-truncate" title="Название название название">
+                        Название название название
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--    случайные фотки. 12 * 3-->
+   <div class="row">
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+       <div class="col-1 my-1">
+           <img src="image/1.jpg" alt="Название" class="img-thumbnail">
+       </div>
+
+   </div>
+
 </div>
 
 
